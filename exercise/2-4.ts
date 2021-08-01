77a63e9e15acee709515acf64b7a4115b9d50df5
asann3 <39186984+asann3@users.noreply.github.com>
2021-08-01 15:30:38 +0900

    TypeScriptの型演習 2-4 途中

--- exercise/2-4.ts
+++ exercise/2-4.ts
@@ -1,5 +1,5 @@
 // type Obj = {}
-function giveId<T>(obj: {[P in | keyof T]: T[P]} ): T {
+function giveId<T>(obj: {[P in keyof T]: T[P]} ): T {
 // function giveId<T>(obj: {foo:number}): T {
 	const id = "本当はランダムがいいけどここではただの文字列";
 	return {
@@ -36,3 +36,60 @@ const obj4: {
 	id: string;
 	name: number;
 } = giveId({ name: "myname" });
+
+interface Foo<T> {
+	[key: string]: T;
+}
+function getters<T>(obj: Foo<T>): {[P in keyof T] : () =>  T } {
+    const result: any = {};
+    for (const key in obj) {
+        result[key] = () => obj[key] as any
+    }
+    return result
+}
+
+// 使用例
+const getters1: {a: () => number, b: () => string} = getters<number | string>({a: 1, b: `x`})
+const getters2: {x: () => Array<number>, y: () => Array<string>} = getters({x: [1], y: [`x`]})
+const getters3: {0: () => string, 1: () => string} = getters({0: `zero`, 1: `one`})
+//　エラー例
+const a: {a: () => string, b: () => number} = getters({a: 1, b: `x`})
+const b: {x: () => Array<string>, b: () => Array<number>} = getters({x: [1], y: [`x`]})
+
+type User2 = {
+	name: string,
+	 age: number
+}
+
+type UserKey = keyof User2;
+// "name" | "age"
+
+type T = number | string
+// keyof A → ? 
+// tostring | valueof 
+type B = keyof T;
+
+// T → number | string
+// keyof T → Tのプロパティ名全ての型 → 36 more ...
+// P → ?
+// typeof P → ?
+type User3 = {
+	name: string,
+	age: number
+}
+
+// typeof
+// JS・TS共通
+const A:string  = "asdf";
+if (typeof A  === "string") {
+	console.log("Yes")
+}
+// TS
+type foo = typeof A;
+// 使用例
+const bar:foo = 'a'
+// エラー例
+const baz:foo = 1
+
+// typeof T → 
+// 
