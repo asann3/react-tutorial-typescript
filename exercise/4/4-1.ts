00728d2efe8bc52dc2f6b1c0e7b921688729307b
asann3 <39186984+asann3@users.noreply.github.com>
2021-08-05 10:02:51 +0900

    TypeScriptの型演習 4-1 途中

---
+++ exercise/4/4-1.ts
@@ -0,0 +1,44 @@
+// interface Obj<T> {
+//     foo?: T
+// }
+
+function getFoo<T>(obj: T): "foo" extends keyof T ? T["foo"] : unknown {
+	return obj.foo;
+}
+
+// T {foo: 123}
+
+// T extends {foo: T} ? T : unknown
+// Tが{foo: T}の部分型
+// T {foo: 123}
+type Z = T extends { foo: T } ? T : unknown;
+type Y = { foo: 123 } extends { foo: { foo: 123 } } ? { foo: 123 } : unknown;
+
+type A = "foo" extends keyof T ? number : unknown;
+type B = "foo" extends keyof {foo: 123} ? number : unknown;
+type C = "foo" extends "foo" ? number : unknown;
+
+
+type ABC = "a" extends string ? undefined : number
+// T {hoge: true}
+
+// 使用例
+// numはnumber型
+const num = getFoo({
+	foo: 123,
+});
+// strはstring型
+const str = getFoo({
+	foo: "hoge",
+	bar: 0,
+});
+// unkはunknown型
+const unk = getFoo({
+	hoge: true,
+});
+
+// エラー例
+getFoo(123);
+getFoo(null);
+
+export {};
