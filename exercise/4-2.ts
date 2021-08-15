d1348a57c5b4c65145d15e181be71ebf358edc32
asann3 <39186984+asann3@users.noreply.github.com>
2021-08-15 15:33:31 +0900

    4-2終了
    Co-authored-by: satackey <me@satackey.com>

---
+++ exercise/4-2.ts
@@ -0,0 +1,33 @@
+function giveId<T>(obj: T): "id" extends T ? T : Pick<T, Exclude<keyof T, 'id'>> & {id: string} {
+	const id = "本当はランダムがいいけどここではただの文字列";
+	return {
+		...obj,
+		id,
+	};
+}
+// (Partial<T> - id: any) & id: strings
+
+// 使用例
+/*
+ * obj1の型は { foo: number; id: string } 型
+ */
+const obj1 = giveId({ foo: 123 });
+declare function acceptObj1(obj: { foo: number; id: string }): void;
+acceptObj1(obj1)
+
+/*
+ * obj2の型は { num : number; id: string } 型
+ */
+const obj2 = giveId({
+	num: 0,
+	id: 100,
+});
+declare function acceptObj2(obj: { num: number; id: string }): void;
+acceptObj2(obj2)
+
+const gotId: string = obj2.id
+// obj2のidはstring型なので別の文字列を代入できる
+obj2.id = '';
+
+
+export{};
